8adea248ebc2fee9b952bddb14f0f3164cc77848
Rafael Pires Gonçalves <127984148+Rafael1572008@users.noreply.github.com>
2025-07-23 20:16:53 -0300

    Hello, World

---
+++ java/src/main/java/br/edu/ifsp/spo/java/EmologApplication.java
@@ -0,0 +1,13 @@
+package br.edu.ifsp.spo.java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmologApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmologApplication.class, args);
+	}
+
+}
