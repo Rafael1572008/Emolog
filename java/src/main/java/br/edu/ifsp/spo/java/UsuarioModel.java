9879bfa8839f11baa3b446dfa293ac54e1f9c14e
Rafael Pires Gonçalves <127984148+Rafael1572008@users.noreply.github.com>
2025-07-26 20:22:48 -0300

    Tabela Usuários

---
+++ java/src/main/java/br/edu/ifsp/spo/java/UsuarioModel.java
@@ -0,0 +1,58 @@
+package br.edu.ifsp.spo.java;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "tb_cadastro_user")
+public class UsuarioModel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    private String nome;
+    private String email;
+    private String senhaHash;
+
+    // Construtores
+    public UsuarioModel() {}
+
+    public UsuarioModel(String nome, String email, String senhaHash) {
+        this.nome = nome;
+        this.email = email;
+        this.senhaHash = senhaHash;
+    }
+
+    // Getters e Setters
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getSenhaHash() {
+        return senhaHash;
+    }
+
+    public void setSenhaHash(String senhaHash) {
+        this.senhaHash = senhaHash;
+    }
+}
