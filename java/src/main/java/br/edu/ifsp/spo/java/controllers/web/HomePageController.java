50a50327b7fe161d15ac0f8e39020c5887b71e7d
ThiagoIFDS23 <129286938+ThiagoIFDS23@users.noreply.github.com>
2025-11-29 17:16:55 -0300

    novo email adicionado
    - email eu@gmail adicionado

--- java/src/main/java/br/edu/ifsp/spo/java/controllers/web/HomePageController.java
+++ java/src/main/java/br/edu/ifsp/spo/java/controllers/web/HomePageController.java
@@ -1,5 +1,7 @@
 package br.edu.ifsp.spo.java.controllers.web;
 
+import br.edu.ifsp.spo.java.model.HumorModel;
+import br.edu.ifsp.spo.java.service.HumorService;
 import br.edu.ifsp.spo.java.service.UsuarioService;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -7,14 +9,18 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import jakarta.servlet.http.HttpSession;
 import org.springframework.ui.Model;
 
+import java.util.List;
+
 @Controller
 @RequestMapping("/")
 public class HomePageController {
     private final UsuarioService usuarioService;
+    private final HumorService humorService;
 
     // Construtor
-    public HomePageController(UsuarioService usuarioService) {
+    public HomePageController(UsuarioService usuarioService, HumorService humorService) {
         this.usuarioService = usuarioService;
+        this.humorService = humorService;
     }
 
     @GetMapping
@@ -24,7 +30,11 @@ public class HomePageController {
         }
 
         String email = (String) session.getAttribute("usuario");
+        Long idUser = usuarioService.findByEmail(email).get().getId();
+
         String nomeUsuario = usuarioService.findByEmail(email).get().getNome();
+//        List<HumorModel> registros = humorService.findByIdUser(idUser);
+
         model.addAttribute("nome", nomeUsuario);
 
         return "index";
